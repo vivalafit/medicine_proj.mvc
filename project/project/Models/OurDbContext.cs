83e973a4f7280d72459e7ea123b8c470337d1a1c
vivalafit <navrotskymax@gmail.com>
2017-08-24 18:49:12 +0300

    Base medicine_proj

---
+++ project/project/Models/OurDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace project.Models
+{
+    public class OurDbContext : DbContext
+    {
+        public DbSet<UserAccount> UserAccount { get; set; }
+    }
+}
---
+++ project/project/Models/UserAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace project.Models
+{
+    public class UserAccount
+    {
+        [Key]
+        public int UserID { get; set; }
+
+        [Required(ErrorMessage ="Введите имя ,пожалуйста .")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Введите фамилию ,пожалуйста .")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Введите е-мейл ,пожалуйста .")]
+        [EmailAddress(ErrorMessage = "Please enter a vaild email.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Введите ник ,пожалуйста .")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Введите пароль ,пожалуйста .")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Compare("Password",ErrorMessage ="Пожалуйста,потдвердите пароль")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+         
+    }
+}
